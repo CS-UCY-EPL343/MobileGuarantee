ac242ccc4ceb3fe678f1140578c8fcd7ec212c92
ssavva05 <ssavva05@cs.ucy.ac.cy>
2016-10-11 22:28:58 +0300

    9

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo '<p>Hello World abc</p>'; 
+        echo '<p>Hello Worl</p>'; 
         ?>
     </body>
 </html>
