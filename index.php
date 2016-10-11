4a356cdb1ae1d3780278c87e451faa6b7b76592a
patis <patis@patis-PC>
2016-10-11 22:24:05 +0300

    8

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo '<p>Hello World</p>'; 
+        echo '<p>Hello World abc</p>'; 
         ?>
     </body>
 </html>
