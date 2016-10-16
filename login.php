a56df5f35dee01d5329cdf0b3c2a96829a5fa876
ssavva05 <ssavva05@cs.ucy.ac.cy>
2016-10-16 13:14:05 +0300

    30

--- login.php
+++ login.php
@@ -51,54 +51,54 @@
                   </div>
               </div>
           </div>-->
-<div class="center-block">
-        <div class="container-fluid">
-            <div class="row center-block">
-                <div class="col-md-12">
-                    <div class="well login-box">
-                        <form action="">
-                            <legend>
-                                <div class="col-md-6">
-                                    <div class="container-fluid">
-                                        <img class="img-responsive" src="pic/logo2.png" alt="Logo" style="width:215px;height:38px" >
+        <div class="center-block">
+            <div class="container-fluid">
+                <div class="row center-block">
+                    <div class="col-md-12">
+                        <div class="well login-box">
+                            <form action="">
+                                <legend>
+                                    <div class="col-md-6">
+                                        <div class="container-fluid">
+                                            <img class="img-responsive" src="pic/logo2.png" alt="Logo" style="width:215px;height:38px" >
+                                        </div>
                                     </div>
+                                    Login
+                                </legend>
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input value='' id="username" placeholder="Username" type="text" class="form-control" />
                                 </div>
-                               Login
-                            </legend>
-                            <div class="form-group">
-                                <label for="username">Username</label>
-                                <input value='' id="username" placeholder="Username" type="text" class="form-control" />
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input id="password" value='' placeholder="Password" type="text" class="form-control" />
-                            </div>
-                            <div class="form-group text-center">
-                                <button class="btn btn-danger btn-cancel-action">Cancel</button>
-                                <input type="submit" class="btn btn-info btn-login-submit" value="Login" />
-                            </div>
-                        </form>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input id="password" value='' placeholder="Password" type="text" class="form-control" />
+                                </div>
+                                <div class="form-group text-center">
+                                    <button class="btn btn-danger btn-cancel-action">Cancel</button>
+                                    <input type="submit" class="btn btn-info btn-login-submit" value="Login" />
+                                </div>
+                            </form>
+                        </div>
+                        <!--
+                        <div class="logged-in well">
+                            <h1>You are loged in! <div class="pull-right"><button class="btn-logout btn btn-danger"><span class="glyphicon glyphicon-off"></span> Logout</button></div></h1>
+                        </div>-->
                     </div>
-                    <!--
-                    <div class="logged-in well">
-                        <h1>You are loged in! <div class="pull-right"><button class="btn-logout btn btn-danger"><span class="glyphicon glyphicon-off"></span> Logout</button></div></h1>
-                    </div>-->
                 </div>
             </div>
         </div>
-</div>
 
         <footer><!-- FOOTER -->
-                 <div class="container">
-                     <p> &emsp; &emsp; </p>
+            <div class="container">
+
                 <?php
                 echo "&copy ";
                 echo date("Y");
                 echo "  MicroPro Systems,Inc";
                 ?>
-                </div>
-            </footer>
-        
+            </div>
+        </footer>
+
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
         <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
     </body>
